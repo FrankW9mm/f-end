1fc35eb87e408da1493f996da1e87fdb522fa788
Frank <thantsithu250703@gmail.com>
2023-03-05 00:41:30 +0630

    changed contents

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,45 @@
 import React from "react";
 import Bg from "../components/img/bg.gif";
+import { BsMouse } from "react-icons/bs";
 const Hero = () => {
   return (
     <div>
-      <div className="flex flex-col bg-[#0E0E0E] p-[3rem] gap-[20px]">
-        <div className="w-full flex flex-row items-center justify-evenly">
-          <div>
-            <h1 className="text-[35px] text-white font-semibold">
-              LOOKING FOR TEST DATA?
+      <div className="flex flex-col md:items-center items-start bg-[#0E0E0E] md:px-[2rem] px-[1rem] py-[3rem] gap-[30px]">
+        <div className="w-full flex lg:flex-row flex-col gap-10 lg:items-center items-start justify-evenly">
+          <div className="flex flex-col text-white gap-2">
+            <h1 className="text-[30px] font-semibold">
+              | LOOKING FOR TEST DATA?
             </h1>
-            <p className="text-white text-[30px]">WE GOT EVERYTHING YOU NEED</p>
+            <p className="text-[21px] font-thin">FROM GENERATING TEXT,IMAGES</p>
+            <p className="text-[21px] font-thin">& COLOR SHADES TO</p>
+            <p className="text-[21px] font-thin">RANDOM ACCESS NOTE</p>
           </div>
-          <div className="text-white">
-            <p className="text-[23px]">FROM GENERATING TEXT,IMAGES</p>
-            <p className="text-[21px]">COLOR SHADE PICKER TO</p>
-            <p className="text-[20px]">RANDOM ACCESS NOTE</p>
-            <p className="text-[18px] text-white">WE GOT ALL U NEED </p>
-          </div>
-          <div>
-            <button className="text-white bg-white/30 backdrop-blur-sm p-[1rem] rounded-[20px]">
-              START NOW
-            </button>
+          <div className=" flex flex-col gap-4">
+            {/* <p className="text-[18px] text-white">WE GOT ALL U NEED </p> */}
+            <p className="text-white text-[18px]">
+              | WE GOT EVERYTHING YOU NEED !
+            </p>
+            <div>
+              <button className="text-white bg-white/30 backdrop-blur-sm p-[1rem] rounded-[20px]">
+                START NOW
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="relative w-full h-[600px]">
-        <div className="absolute w-full h-[600px] flex justify-center items-center bg-black/60">
-          <h1 className="text-white text-[100px] font-bold translate-x-10">
-            F-ENDZ
+      <div className="relative w-full h-[700px]">
+        <div className="absolute w-full h-[700px] flex-col flex justify-center items-center bg-black/80">
+          <p className="text-white md:translate-x-10 translate-x-0 uppercase">
+            Introducing...
+          </p>
+          <h1 className="text-white lg:text-[100px] md:text-[70px] mb-[20px] text-[50px] font-bold md:translate-x-10 translate-x-0">
+            | F-ENDZ |
           </h1>
+          <BsMouse
+            size={40}
+            color={"white"}
+            className=" md:translate-x-10 translate-x-0"
+          />
         </div>
         <img
           className="w-full h-full object-top object-cover"
--- src/components/Navigate.jsx
+++ src/components/Navigate.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navigate = () => {
   return (
-    <div className="flex flex-row justify-between bg-[#0E0E0E] p-[2rem] ">
+    <div className="flex flex-row justify-between bg-[#0E0E0E] md:px-[3rem] px-[1rem] md:py-[2rem] py-[1rem] shadow-2xl ">
       <div>
-        <h1 className="text-white text-[32px]">LOGO</h1>
+        <h1 className="text-white text-[32px]">| F-ENDZ |</h1>
       </div>
       <div>
         <button className="p-[1rem]  rounded-[20px] bg-white/10 text-white backdrop-blur-3xl">
-          START NOW
+          Explore now
         </button>
       </div>
     </div>
